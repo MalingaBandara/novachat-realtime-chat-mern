b69b7c2a132af9f7ff2190b70fa2f65cb020eb3b
Malinga Bandara <malinga1021@gmail.com>
2026-02-25 17:12:16 +0530

    feat(auth): implement user login route with JWT authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const express = require("express"); //? Express framework for handling HTTP requ
 const User = require("../models/UserModel"); //? User model to interact with users collection
 const userRouter = express.Router(); //? Creating a router instance
 const jwt = require("jsonwebtoken"); //? Library used for generating authentication tokens (JWT)
+const { isValidElement } = require("react");
 
 
 //* ============================================
@@ -41,3 +42,59 @@ userRouter.post('/register', async (req, res) => {
 });
 
 
+
+//* ============================================
+//* USER LOGIN ROUTE
+//* ============================================
+//! Handles POST request to authenticate user
+userRouter.post('/login', async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body; //* Extract login credentials from request body
+
+        const user = await User.findOne({ email }); //* Find user by email
+
+        //* Validate:
+//? 1. User exists
+//? 2. Entered password matches hashed password
+        if (user && (await user.matchPassword(password))) {
+
+            //* If authentication successful, return user data + JWT token
+            res.json({
+                user: {
+                    _id: user._id,
+                    username: user.username,
+                    email: user.email,
+                    isAdmin: user.isAdmin, //? Indicates admin privileges
+                    token: generateToken(user._id) //! Generate signed JWT token
+                },
+            });
+
+        } else {
+            res.status(401).json({ message: "Invalid email or password" }); //! If email or password is incorrect
+        }
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });//! Handle unexpected server errors
+    }
+
+});
+
+
+
+//* ============================================
+//* JWT TOKEN GENERATOR FUNCTION
+//* ============================================
+//! Creates a signed JSON Web Token
+//? Used for authenticating protected routes
+//? Token contains user ID inside payload
+const generateToken = (id) => {
+
+    return jwt.sign(
+        { id }, //? Payload (data stored inside token)
+        process.env.JWT_SECRET, //? Secret key from environment variables
+        { expiresIn: "30d" } //? Token expiration time
+    );
+
+};
